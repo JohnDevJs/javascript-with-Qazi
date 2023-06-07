3e3386b301dafbf5dbf4560e8e4542c5f965117a
JohnDevJs <JohnDevJs@users.noreply.github.com>
2023-06-07 22:08:26 +0200

    fighting games

---
+++ Bank/style.css
@@ -0,0 +1,16 @@
+class Bank {
+    constructor(balance) {
+      this.balance = balance;
+    }
+  
+    withdraw(amount) {
+      this.balance -= amount;
+    }
+  
+    deposit(amount) {
+      this.balance += amount;
+    }
+  }
+  
+  const qaziChecking = new Bank(100);
+  console.log(qaziChecking.balance);
